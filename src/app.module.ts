c21a6e6ffae0125b561d2b268cc24915d1ce982b
Chris LeMatty <clematty@gmail.com>
2020-07-04 14:59:07 +0200

    setup docker compose with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConnectionService } from './database-connection/database-connection.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConnectionService,
+    })
+  ],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, DatabaseConnectionService],
 })
 export class AppModule {}
